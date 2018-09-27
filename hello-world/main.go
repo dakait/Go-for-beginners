200713da1c9f4eef03f8082ca3c481d96bdc5294
dakait <manishksharmacse@gmail.com>
2018-09-27 11:09:20 +0530

    hello world

---
+++ hello-world/main.go
@@ -0,0 +1,12 @@
+package main 		//you must include a package; main to run it as main package 
+
+import "fmt"
+
+func main() {
+	//Package fmt implements formatted I/O with functions analogous to C's printf and scanf
+	fmt.Printf("Hello world !!")
+
+	//for new line after your string use
+	fmt.Println("Hello world!!")	
+}
+
